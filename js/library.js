956f91a83c795683fc2a48933e73d04693a26cbf
Justin <Justin.nthai@gmail.com>
2022-08-01 20:07:44 -0700

    Remove html placeholder cards, Adjust all data attributes to include the data tag in html and js, Set sample book as first book object in library array, Fix some js logic to consolidate methods to avoid scope problems and other critcal issues affecting the book card functioning as it should

--- js/library.js
+++ js/library.js
@@ -1,13 +1,22 @@
-const bookContainer = document.querySelector('[book-card-container]');
-const addBookBtn = document.querySelector('[add-book]');
-const formTitle = document.querySelector('[form-title]');
-const formAuthor = document.querySelector('[form-author]');
-const formPages = document.querySelector('[form-pages]');
-const formLang = document.querySelector('[form-lang]');
-const formPub = document.querySelector('[form-pub]');
-const formReadStatus = document.querySelector('[form-read-status]');
+const bookContainer = document.querySelector('[data-book-card-container]');
+const addBookBtn = document.querySelector('[data-add-book]');
+const formTitle = document.querySelector('[data-form-title]');
+const formAuthor = document.querySelector('[data-form-author]');
+const formPages = document.querySelector('[data-form-pages]');
+const formLang = document.querySelector('[data-form-lang]');
+const formPub = document.querySelector('[data-form-pub]');
+const formReadStatus = document.querySelector('[data-form-read-status]');
 
-let myLibrary = [];
+let myLibrary = [
+  {
+    formTitle: 'Think Like a Programmer: An Introduction to Creative Problem Solving',
+    formAuthor: 'V. Anton Spraul',
+    formPages: '256',
+    formLang: 'English',
+    formPub: 'August 12 2012',
+    formReadStatus: 'read'
+  }
+];
 
 class Book {
   constructor(formTitle, formAuthor, formPages, formLang, formPub, formReadStatus) {
@@ -18,51 +27,80 @@ class Book {
     this.formPub = formPub.value;
     this.formReadStatus = formReadStatus.value;
   }
-
-  addBook() {
-    const convertHtml = (html) => {
-      const template = document.createElement('template');
-      template.innerHTML = html.trim();
-      return template.content.firstElementChild;
+  
+  displayBook() {
+    while (bookContainer.firstChild) {
+      bookContainer.removeChild(bookContainer.lastChild);
     }
-    const sanitizeHtml = (str) => {
-      const temp = document.createElement('div');
-      temp.textContent = str;
-      return temp.innerHTML;
-    }
-    const book_html_template = `
-      <div class="book-card">
-      <span class="card-remove">+</span>
-      <h3 class="book-title">${sanitizeHtml(this.formTitle)}</h3>
-      <span class="book-author">
-        <span class="book-label">Author:</span>
-        ${sanitizeHtml(this.formAuthor)}
-      </span>
-      <span class="book-pages">
-        <span class="book-label">Page Count:</span>
-        ${sanitizeHtml(this.formPages)}
-      </span>
-      <span class="book-language">
-        <span class="book-label">Language:</span>
-        ${sanitizeHtml(this.formLang)}
-      </span>
-      <span class="book-publication">
-        <span class="book-label">Published:</span>
-        ${sanitizeHtml(this.formPub)} 
-      </span>
-      <span class="read-toggle-label">Mark As Read:</span>
-      <label class="toggle-switch">
-        <input type="checkbox" ${sanitizeHtml(this.formReadStatus) === 'read' ? 'checked' : 'unchecked'}>
-        <span class="toggle-slider"></span>
-      </label>
-    </div>`;
-    bookContainer.append(convertHtml(book_html_template));
-    myLibrary.push(convertHtml(book_html_template));
 
-  } 
+    let i = 0;
+    myLibrary.forEach((books) => {
+      if (books.formTitle === '' || books.formAuthor === '' || books.formPages === '' || 
+      books.formLang === '' || books.formPub === '' || books.formReadStatus=== '' ) return //try to do this check later before adding to the array, testing for now
+
+      const convertHtml = (html) => {
+        const template = document.createElement('template');
+        template.innerHTML = html.trim();
+        return template.content.firstElementChild;
+      }
+      const sanitizeHtml = (str) => {
+        const temp = document.createElement('div');
+        temp.textContent = str;
+        return temp.innerHTML;
+      }
+      const book_html_template = `
+        <div class="book-card">
+          <span class="card-remove" data-close-card=${i}>+</span>
+          <h3 class="book-title">${sanitizeHtml(books.formTitle)}</h3>
+          <span class="book-author">
+            <span class="book-label">Author:</span>
+            ${sanitizeHtml(books.formAuthor)}
+          </span>
+          <span class="book-pages">
+            <span class="book-label">Page Count:</span>
+            ${sanitizeHtml(books.formPages)}
+          </span>
+          <span class="book-language">
+            <span class="book-label">Language:</span>
+            ${sanitizeHtml(books.formLang)}
+          </span>
+          <span class="book-publication">
+            <span class="book-label">Published:</span>
+            ${sanitizeHtml(books.formPub)} 
+          </span>
+          <span class="read-toggle-label">Mark As Read:</span>
+          <label class="toggle-switch">
+            <input type="checkbox" ${sanitizeHtml(books.formReadStatus) === 'read' ? 'checked' : 'unchecked'}>
+            <span class="toggle-slider"></span>
+          </label>
+        </div>`;
+
+      bookContainer.append(convertHtml(book_html_template));
+
+      const closeCard = document.querySelector(`[data-close-card='${i}']`); 
+      const removeCard = () => {
+        let removePos = closeCard.dataset.closeCard;
+        myLibrary.splice(parseInt(removePos), 1);
+        this.displayBook();
+      }
+      closeCard.addEventListener('click', () => {
+        removeCard();
+      })
+      i++;
+    })
+
+  } // things to do: link read toggle to class, button to open/close form, link/count for books in library.
 
   delBook() {
-
+    removeCard
+/*    document.querySelector(`['${this.dataset.close}']`).onclick = function () {
+      this.parentNode.remove();
+      return false;
+    }; */
+/*     const closeCard = document.querySelector(`[close-card='${closeCard.dataset.closeCard}']`);
+    let removeCard = closeCard.dataset.closeCard;
+    myLibrary.splice(parseInt(removeCard, 1)); */
+    
   }
 
   statusBook() {
@@ -73,8 +111,17 @@ class Book {
 
 }
 
+
+const addBookToLibrary = () => {
+  let book = new Book(formTitle, formAuthor, formPages, formLang, formPub, formReadStatus);
+  myLibrary.push(book);
+  book.displayBook();
+}
+
+window.onload = () => addBookToLibrary();
+
 addBookBtn.addEventListener('click', () => {
- const book = new Book(formTitle, formAuthor, formPages, formLang, formPub, formReadStatus);
- myLibrary.push(book)
- book.addBook();
+ addBookToLibrary();
 })
+
+// debate on auto fit vs auto fill
